0b9f685fbf70ee5439f76234b7e59a70028665e4
oluwamayowaf <e_fadairo@yahoo.com>
2022-04-05 18:39:48 +0100

    (ft) updates to the search form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,15 +4,27 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { deepPurple } from '@mui/material/colors';
+import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: deepPurple
+  },
+});
 
 const App = () => {
   return (
-   
+    <ThemeProvider theme={theme}>
+      <Header />
      <Container> 
+       <SearchForm />
      </Container> 
-   
+     </ThemeProvider>
   );
 };
 
